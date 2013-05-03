643b714d869b37a3444798465a3b86cf789d9ffb
Martin Xu <Martin.Xu@move.com>
2013-05-03 14:37:05 -0700

    added all point tests and passed

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -15,4 +15,30 @@ describe "Point" do
     p1.compare(p0).should == 1
   end
 
+  it 'test equality' do
+    p0 = Point.new(1,1)
+    p0.compare(p0).should == 0
+  end
+
+  it 'test left of line' do
+    p0 = Point.new(1,1)
+    p1 = Point.new(3,3)
+    p2 = Point.new(1,3)
+    p2.is_left(p0,p1).should > 0
+  end
+
+  it 'test right of line' do
+    p0 = Point.new(1,1)
+    p1 = Point.new(3,3)
+    p2 = Point.new(3,1)
+    p2.is_left(p0,p1).should < 0
+  end
+
+  it 'test on of line' do
+    p0 = Point.new(1,1)
+    p1 = Point.new(3,3)
+    p2 = Point.new(2,2)
+    p2.is_left(p0,p1).should == 0
+  end
+
 end
