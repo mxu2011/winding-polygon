86bc52fbaadcd3d59b9b309d4ca62e4738ce0782
Martin Xu <Martin.Xu@move.com>
2013-05-06 09:18:08 -0700

    changed the way to load the library for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-Dir["../lib/winding-polygon/*.rb"].each {|file| require file }
+$LOAD_PATH.unshift File.expand_path('../../lib/winding-polygon', __FILE__)
+#Dir["../../lib/winding-polygon/*.rb"].each {|file| require file }
+require 'avltree.rb'
+require 'event_queue'
+require 'point'
+require 'polygon'
+require 'segment'
+require 'sweep_line'
 require 'winding-polygon'
 require 'json'
 require "rspec"
